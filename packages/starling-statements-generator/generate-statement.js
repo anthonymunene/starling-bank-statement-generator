d10ebb2d234b9e8c49db944f3561cd90f49381bc
anthonymunene <munene.anthony@gmail.com>
2023-10-30 00:04:00 +0000

    return something

--- packages/starling-statements-generator/generate-statement.js
+++ packages/starling-statements-generator/generate-statement.js
@@ -1,12 +1,13 @@
 async function main(args) {
   try {
     const url = `https://openlibrary.org/search.json?q=harry+potter`;
-    const harryPotterBooks = await fetch(url)
+    const harryPotterBooks = await fetch(url);
     const results = await harryPotterBooks.json();
 
-    const titles = results?.docs.map(result => result.title)
-    const uniqueTitles = [...new Set(titles)]
+    const titles = results?.docs.map((result) => result.title);
+    const uniqueTitles = [...new Set(titles)];
     console.log(uniqueTitles);
+    return { "body": titles };
   } catch (error) {
     console.log(error);
   }
