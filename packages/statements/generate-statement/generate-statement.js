558f48de252f894d1fedf32ec8c1c2f43e2f5699
anthonymunene <munene.anthony@gmail.com>
2024-09-02 16:28:11 +0300

    add specific error messages

--- packages/statements/generate-statement/generate-statement.js
+++ packages/statements/generate-statement/generate-statement.js
@@ -5,9 +5,10 @@ import { appConfig} from "./service/variables.js";
 
 export const main = async () => {
     await generatePDF().then(async pdf => {
-        await sendEmail(appConfig.recipient, appConfig.subject, pdf).then(result => {
-            return {body: `email sent to ${result.envelope.to[0]}`}
-        })
+        return {body: `email sent to ${pdf}`}
+        // await sendEmail(appConfig.recipient, appConfig.subject, pdf).then(result => {
+        //     return {body: `email sent to ${result.envelope.to[0]}`}
+        // })
     }).catch(error => {
         console.error(`generateStatement Error: ${error}`)
     })
